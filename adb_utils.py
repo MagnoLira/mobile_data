5215aa5dc94472bd701b2de158a73012329e5636
Magno Lira <104531871+MagnoLira@users.noreply.github.com>
2025-01-17 23:02:01 -0300

    Update adb_utils.py
    ~ Adjusting comments

--- adb_utils.py
+++ adb_utils.py
@@ -1,20 +1,20 @@
 import subprocess
 
-# Função para executar comandos ADB e capturar a saída
+# Exec adb commands 
 def run_adb_command(command):
     try:
         result = subprocess.run(command, capture_output=True, text=True, shell=True)
         return result.stdout
     except Exception as e:
-        print(f"Erro ao executar comando ADB: {e}")
+        print(f"Error on abd command: {e}")
         return ""
 
-# Função para coletar dados de uso de aplicativos
+# Collect info on usage apps - daily 
 def get_app_usage():
     output = run_adb_command('adb shell dumpsys usagestats --include-history')
     return output
 
-# Função para coletar informações da bateria
+# Collect battery status
 def get_battery_status():
     output = run_adb_command('adb shell dumpsys battery')
     return output
